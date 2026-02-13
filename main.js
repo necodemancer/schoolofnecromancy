8ab995e38df83acb5940bdf0b6c2129db4c40f65
Necromancer Coding <96705309+necodemancer@users.noreply.github.com>
2026-02-13 09:26:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 $(function(){
 $('body').prepend('<button class="lang-switch" data-lang="es">Español</button><button class="lang-switch" data-lang="en">English</button>');
+
+// Event listener for buttons
+$(".lang-switch").click(function() {
+  const lang = $(this).data("lang");
+  changeLanguage(lang);
+});
+
+});
+
+function changeLanguage(lang) {
+  $.getJSON(`lang/${lang}.json`, function(data) {
+    $("[data-i18n]").each(function() {
+      const key = $(this).data("i18n");
+      $(this).text(data[key]);
+    });
+    localStorage.setItem("selectedLang", lang);
+    $('body').attr('lang', localStorage.getItem("selectedLang"));
+  });
+}
+
+$(document).ready(function() {
+  const savedLang = localStorage.getItem("selectedLang") || "es";
+  changeLanguage(savedLang);
 });
