200f7d33fbfc07680cc4ee439477a080da8590c3
Necromancer Coding <96705309+necodemancer@users.noreply.github.com>
2026-07-30 16:39:38 +0200

    Update truesight-groupcolors.js

--- truesight/truesight-groupcolors.js
+++ truesight/truesight-groupcolors.js
@@ -1,6 +1,7 @@
 /* ==========================================================================
    Truesight - Custom Tooltip System
    Author: Necromancer Coding
+   Version: 1.2 // Group Colors Vr.
    ========================================================================== */
 
 $(function () {
